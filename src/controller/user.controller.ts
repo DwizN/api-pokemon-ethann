d3f80d4040c9401f49322c3889c1c8f7cb97b7ad
Lieserl V <113937088+lieserlV@users.noreply.github.com>
2025-02-04 10:04:25 +0100

    ajout des routes User

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -77,3 +77,81 @@ export const loginUser = async (req: any, res: any) => {
   res.status(201).json({ message: `Bienvenue ${email} !`, token });
 
 };
+
+export const updateUser = async (req: any, res: any) => {
+    const {olddata, newdata} = req.body;
+
+  if (!olddata) {
+    return res.status(400).send({ error: 'Erreur 400 Bad Request :  Le champ olddata est requis.' });
+  }
+
+  if (!newdata) {
+    return res.status(400).send({ error: 'Erreur 400 Bad Request :  Le champ newdata est requis.' });
+  }
+
+  const existingUser = await verifyexistingUserReturn(olddata, res, () => {}); // On vérifie si l'utilisateur existe
+  if (res.statusCode === 400) {
+    return res;
+  }
+
+  // Vérification du mot de passe
+  if ('password' in existingUser) {
+    const match = await bcrypt.compare(olddata.password, existingUser.password);
+    if (!match) {
+      return res.status(400).send({ error: 'Erreur 400 Bad Request : Le mot de passe est incorrect' });
+    }
+  } else {
+    return res.status(500).send({ error: 'Erreur 500 : Une erreur interne s"est produite.' });
+  }
+
+  try {
+    // Retire les valeurs null de newdata pour ne pas les écraser
+    const updatedData = Object.fromEntries(
+      Object.entries(newdata).filter(([_, value]) => value !== null)
+    );
+
+    const updatedUser = await prisma.user.update({
+      where: { email: olddata.email },
+      data: updatedData,
+    });
+    res.status(200).send(`Utilisateur ${updatedUser.email} mis à jour`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Erreur 500 : Une erreur interne s"est produite.');
+  }
+};
+
+export const deleteUser = async (req: any, res: any) => {
+    const {email, password} = req.body;
+
+  if (!email) {
+    return res.status(400).send({ error: 'Erreur 400 Bad Request :  Le champ email est requis.' });
+  }
+
+  if (!password) {
+    return res.status(400).send({ error: 'Erreur 400 Bad Request :  Le champ password est requis.' });
+  }
+
+  const existingUser = await verifyexistingUserReturn(req, res, () => {}); // On vérifie si l'utilisateur existe
+  if (res.statusCode === 400) {
+    return res;
+  }
+
+  // Vérification du mot de passe
+  if ('password' in existingUser) {
+    const match = await bcrypt.compare(password, existingUser.password);
+    if (!match) {
+      return res.status(400).send({ error: 'Erreur 400 Bad Request : Le mot de passe est incorrect' });
+    }
+  } else {
+    return res.status(500).send({ error: 'Erreur 500 : Une erreur interne s"est produite.' });
+  }
+
+  try {
+    await prisma.user.delete({ where: { email: email } });
+    res.status(204).send(`Utilisateur ${email} supprimé`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Erreur 500 : Une erreur interne s"est produite.');
+  }
+};
--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createUser, loginUser } from '../controller/user.controller';
+import { createUser, loginUser, updateUser, deleteUser } from '../controller/user.controller';
+import { verifyJWT } from '../services/user.service';
 
 const userRouter = Router();
 
 userRouter.post('/users', createUser);
 userRouter.post('/users/login', loginUser);
+userRouter.patch('/users', verifyJWT, updateUser);
+userRouter.delete('/users', verifyJWT, deleteUser);
 
 export const userRoutes = () => {
     return userRouter;
